22f02c6ab343f49e040c4ad8c6b9390193b889d4
a-levyakov <a.levyakov95@gmail.com>
2025-03-06 20:52:23 +0300

    1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+
+
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args)  {
+        UserService UserService = new UserServiceImpl();
+
+        try {
+            UserService.createUsersTable();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        UserService.saveUser("Name1", "LastName1", (byte) 11);
+        UserService.saveUser("Name2", "LastName2", (byte) 22);
+        UserService.saveUser("Name3", "LastName3", (byte) 33);
+        UserService.saveUser("Name4", "LastName4", (byte) 44);
+
+        UserService.removeUserById(1);
+        UserService.getAllUsers();
+        UserService.cleanUsersTable();
+        UserService.dropUsersTable();
     }
 }
